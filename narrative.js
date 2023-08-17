e460418ff8da02200cec21e167142640f33517e8
jessicahattig <jessicahattig@gmail.com>
2023-08-16 20:52:54 -0700

    WIP: narrative logic.

--- narrative.js
+++ narrative.js
@@ -71,4 +71,29 @@ class Narrative {
   }
 }
 
-class 
+class AdventureGame {
+  constructor(playerName) {
+    this.playerName = playerName;
+    this.points = 0;
+    this.currentStoryKey = 'start';
+    this.stories = this.initializeStories();
+  }
+
+  initializeStories() {
+    return {
+      'start' : this.createStory('You are + this.playerName +, a young eco-warrior standing against the environmentally destructive Monsanto Corporation. What will you do?', [
+        this.createChoice('Sneak into a Monsanto facility to gather information?', mansanto1, 10),
+        this.createChoice('eek out the eco-resistance in the nearby forest?', eco1, 20)
+      ]),
+      monsanto1: this.createStory('You manage to bypass security and enter the facility, but it\'s crawling with guards. What\s your next move?', [
+        this.createChoice('Try to hack into their mainframe for evidence?', monsanto2A, 15),
+        this.createChoice('Look for a physical file or documents?', monsanto2b, 25)
+      ]),
+      monsanto1: this.createStory('You manage to bypass security and enter the facility, but it\'s crawling with guards. What\s your next move?', [
+        this.createChoice('Try to hack into their mainframe for evidence?', monsanto2A, 15),
+        this.createChoice('Look for a physical file or documents?', monsanto2b, 25)
+      ]),
+
+    }
+  }
+}
