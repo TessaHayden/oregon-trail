34673c56d85189d959dd563e0e6bbc724d500afd
jessicahattig <jessicahattig@gmail.com>
2023-08-20 14:52:02 -0700

    WIP: narrative business logic.

--- src/narrative.js
+++ src/narrative.js
@@ -56,20 +56,20 @@ class StoryPrompt{
   constructor(choice) {
     this.storyData = [
       { //index 0
-        text: "EcoGuard: Corporate Takedown You are Alex, a young eco-warrior standing against the environmentally destructive Monsanto Corporation. Start: You're in the heart of the city, watching as smog clouds the skyline. Monsanto's logo is everywhere,",
+        text: "Corporate Takedown You are Alex, a young eco-warrior standing against the environmentally destructive Monsanto Corporation. Start: You're in the heart of the city, watching as smog clouds the skyline. Monsanto's logo is everywhere,",
         choices: [
           {text: "Sneak into a Monsanto facility to gather information?", arrayIndex: 1 },
           {text: "Seek out the eco-resistance in the nearby forest?", arrayIndex: 2 }
         ]
       },
-      { //index 1
+      { //index 1 from 0{0}
         text: "Sneak into Monsanto facility: You manage to bypass security and enter the facility, but it's crawling with guards.",
         choices: [
           {text: "Try to hack into their mainframe for evidence?", arrayIndex: 3 },
           {text: "Look for a physical file or documents?", arrayIndex: 4 }
         ]
       },
-      { //index 2
+      { //index 2 from 0[1]
         text: "Seek out the eco-resistance: You meet a group of environmentalists. They offer you tools and gadgets to help",
         choices: [
           {text: "Accept their tech and plan a joint sabotage mission", arrayIndex: 5 },
@@ -80,31 +80,32 @@ class StoryPrompt{
       text: "While hacking the Monsanto mainframe, you're caught while hacking and are thrown into a cell",
         choices: [
           {text: "Try to escape and risk being caught", arrayIndex: 7},
-          {text: "Try ?", arrayIndex: 8 }
+          {text: "Wait for an opportunity and ally to help?", arrayIndex: 8 }
         ]
       },
-      { //index 4
+      { //index 4 from 1[1]
       text: "While looking for evidence at the facility, you find incriminating documents about their operations",
         choices: [
-          {text: "Leak it to the media immediately?", arrayIndex: 5},
-          {text: "Share it with the resistance?", arrayIndex: 6 }
+          {text: "Leak it to the media immediately?", arrayIndex: 9},
+          {text: "Share it with the resistance for a strategic reveal?", arrayIndex: 10 }
         ]
       },
       { //index 5 from 2[0]
-      text: "Sabotage Mission, .you and eco-resistance plan a mass protest against Monsanto ",
+      text: "Sabotage Mission",
         choices: [
-          {text: "Accept their tech and plan a joint sabotage mission?", arrayIndex: 7},
-          {text: "Decline and decide to gather more allies from the city?", arrayIndex: 8 }
+          {text: "Lay low and avoid confrontation", arrayIndex: 11},
+          {text: "Prepare for an imminent Monsanto retaliation?"
+          arrayIndex: 12}
         ]
       },
       { //index 6 from 2[1]
-      text: "While looking for evidence at the facility, you find incriminating documents about their operations",
+      text: "As you gather more allies from the city, do you",
         choices: [
-          {text: "Leak it to the media immediately?", arrayIndex: 5},
-          {text: "Share it with the resistance?", arrayIndex: 6 }
+          {text: "Plan a mass protest against Monsanto?", arrayIndex: 13},
+          {text: "Use the crowd as a diversion while a smaller group infiltrates Monsanto?", arrayIndex: 14}
         ]
       },
-      { //index 7
+      { //index 7 
       text: "While pulling off the sabotage operation, you succeed but gather lots of attention",
         choices: [
           {text: "Lay low?", arrayIndex: 5},
@@ -113,6 +114,20 @@ class StoryPrompt{
       },
       { //index 8
       text: "While looking for evidence at the facility, you find incriminating documents about their operations",
+        choices: [
+          {text: "Accept their tech and plan a joint sabotage mission?", arrayIndex: 5},
+          {text: "Decline and decide to gather more allies from the city?", arrayIndex: 6 }
+        ]
+      },
+      { //index 9
+      text: "While Leaking it to the media immediately",
+        choices: [
+          {text: "Lay low?", arrayIndex: 5},
+          {text: "Prepare for Retaliation", arrayIndex: 6 }
+        ]
+      },
+      { //index 10
+      text: "While Share it with the resistance for a strategic reveal?",
         choices: [
           {text: "Accept their tech and plan a joint sabotage mission?", arrayIndex: 5},
           {text: "Decline and decide to gather more allies from the city?", arrayIndex: 6 }
