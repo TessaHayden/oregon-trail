11b57212ff52ddee1ed83e982b4d9d264ece8a8a
TessaHayden <tessahayden3@gmail.com>
2023-08-21 20:47:02 -0700

    WIP, integrating index.js

--- src/index.js
+++ src/index.js
@@ -2,48 +2,46 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import "animate.css";
-// import Narrative from "./narrative";
+import Narrative from "./narrative";
 
 function animateCSS() {
-    let x = document.getElementById("animate");
-    x.style.marginLeft = "300px";
-    x.style.transition = "1s";
+  let x = document.getElementById("animate");
+  x.style.marginLeft = "85%";
+  x.style.transition = "1.5s";
 }
 
 function showOption(event) {
+  // story.printCurrentStep();
+  // story.makeChoice();
   event.preventDefault();
-  let narrative = new Narrative();
-  let userChoice = narrative.makeChoice(radioValue);
-  console.log(userChoice);
-  let textElement = document.querySelector("#story-text");
-  const promptElementA = document.querySelector("#prompt-a");
-  promptElementA.innerText =
-    "Sneak into a Monsanto facility to gather information?";
-  const promptElementB = document.querySelector("#prompt-b");
-  promptElementB.innerText =
-    "Seek out the eco-resistance in the nearby forest?";
-  textElement.innerText = null;
   const radioValue = document.querySelector(
     "input[name='story-options']:checked"
   ).value;
+  let narrative = new Narrative();
+  let textElement = document.querySelector("#story-text");
+  const promptElementA = document.querySelector("#prompt-a");
+  promptElementA.innerText = narrative.storyData[narrative.currentStep].choices[0].text;
+  const promptElementB = document.querySelector("#prompt-b");
+  promptElementB.innerText = narrative.storyData[narrative.currentStep].choices[1].text;
+  textElement.innerText = null;
   if (radioValue === "0") {
-    textElement.innerText = "hi";
+    textElement.innerText = narrative.storyData[narrative.currentStep].text;
   }
   if (radioValue === "1") {
-    textElement.innerText = "narrativeVal";
+    textElement.innerText = narrative.storyData[narrative.currentStep].text;
   }
 }
 
-  // function findStoryPath() {
-  //   let narrative = new Narrative();
-  //   console.log(narrative);
-  //   let value = document.querySelector("input[name='story-options']:checked");
-  //   let storyLineIdx0 = this.storyData[0].choices[0];
-  //   console.log(storyLineIdx0);
-  //   if (storyLineIdx0 && value === "a") {
-  //     console.log("working");
-  //   }
-  // }
+// function findStoryPath() {
+//   let narrative = new Narrative();
+//   console.log(narrative);
+//   let value = document.querySelector("input[name='story-options']:checked");
+//   let storyLineIdx0 = this.storyData[0].choices[0];
+//   console.log(storyLineIdx0);
+//   if (storyLineIdx0 && value === "a") {
+//     console.log("working");
+//   }
+// }
 
 window.addEventListener("load", function () {
   const form = document.querySelector("form");
